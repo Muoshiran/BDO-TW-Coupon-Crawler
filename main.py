41936ce681fc62ecd6ffc10b0fbfc7e268da8a9d
Muoshiran <110709347+Muoshiran@users.noreply.github.com>
2022-12-31 13:49:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,36 @@ import re
 def crawlPage(pg=1):
     url = "https://forum.gamer.com.tw/C.php?bsn=19017&snA=58772&page="+str(pg)
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    return response.text
+    return response.text  # str
     
 def rm(pg):
     d = []
-    each_floor = pg.find_all(class_=["c-article__content", "comment_content"])
-    for afloor in each_floor:
-        for aline in afloor:
-            aline = re.findall(("[A-Za-z0-9!]"*4 + "-?")*3+"[A-Za-z0-9!]"*4, str(aline.text))
-            for result in aline:
-                if result.count("-") in (0, 3):
-                    d += [result]
-    return d
+    each_floor = str(pg.find_all(class_=["c-article__content", "comment_content"]))
+    # Seperate the contents of the html tag into single line and remove the tag
+    while True:
+        start_tag = each_floor.find("<")
+        end_tag = each_floor.find(">")
+
+        if start_tag not in [None, -1] and end_tag not in [None, -1]:
+            each_floor = each_floor.replace(each_floor[start_tag:end_tag+1], "\n")  # <.+?> to \n
+            start_tag = end_tag = None  # reset the index
+
+        if start_tag == -1 or end_tag == -1:  # break when there are no < >
+            break
+    
+    for result in re.findall(("[A-Za-z0-9!]"*4 + "-?")*3+"[A-Za-z0-9!]"*4, each_floor):
+        if result.count("-") in (0, 3):  # avoid weird result  aaaa-aaaa3234wsax
+            result = result.upper()
+
+            if len(result) == 16:  # not follow the format
+                result = result[0:4] + "-" + result[4:8] + "-" + result[8:12] + "-" + result[12:]  # 5, 9, 13 is -
+            if result not in d:  # avoid duplicate
+                d += [result]
+    return d  # list
 
 def main():
-    pg = crawlPage() + crawlPage(99999)
-    d = rm(BeautifulSoup(pg, "html.parser"))
-    d = list(set(d))
-    for idx, cnt in enumerate(d):
-        d[idx] = cnt.upper()
-        if len(cnt) == 16:
-            d[idx] = cnt[0:4] + "-" +cnt[4:8] + "-" + cnt[8:12] + "-" + cnt[12:]
+    pg = crawlPage() + crawlPage(99999)  # return str
+    d = rm(BeautifulSoup(pg, "html.parser"))  # return list
     print(d)
         
     
